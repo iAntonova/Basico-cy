c94507b3784e2d7638284f706a7ae5c9cdad5b72
iAntonova <antonovava@gmail.com>
2023-03-21 09:00:12 +0200

    promise

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Basico-cy
+
+npx cypress run --spec "cypress/e2e/gianni_course/**/*.cy.js"
+
+npx cypress run --spec "cypress/e2e/gianni_course/webdriver-uni/*"
--- cypress/e2e/gianni_course/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/gianni_course/webdriver-uni/contact-us.cy.js
@@ -3,20 +3,25 @@
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include', 'WebDriver | Contact Us')
+        cy.url().should('include', 'contactus')
         //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type('Joe')
         cy.get('[name="last_name"]').type('Smith')
         cy.get('[name="email"]').type('test@gmail.com')
         cy.get('textarea.feedback-input').type('Lorem ipsom ... ')
         cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
     })
 
-    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
         cy.get('[name="first_name"]').type('Tom')
         cy.get('[name="last_name"]').type('Smith')
         cy.get('textarea.feedback-input').type('Lorem ipsom ... ')
         cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
     })
 })
 
