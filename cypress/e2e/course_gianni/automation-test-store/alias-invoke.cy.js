6c548153b594e222637c61ced444ea9931681dd9
iAntonova <antonovava@gmail.com>
2023-03-22 18:31:46 +0200

    25: wrap up of using: alias, invoke, variables

--- cypress/e2e/course_gianni/automation-test-store/alias-invoke.cy.js
+++ cypress/e2e/course_gianni/automation-test-store/alias-invoke.cy.js
@@ -10,21 +10,69 @@ describe("Alias and invoke", () => {
             .eq(0)
             .invoke('text')
             .as('productThumbnail')
-        
+
         cy.get('@productThumbnail').its('length').should('be.gt', 5)
         cy.get('@productThumbnail').should('include', 'Seaweed Conditioner')
     })
 
-    it.only("Validate product thumbnail", () => {
+    it("Validate product thumbnail", () => {
         cy.visit('https://www.automationteststore.com/')
         cy.get('.thumbnail').as('productThumbnail')
-                       
+
         cy.get('@productThumbnail')
-        .should('have.length', 16)
+            .should('have.length', 16)
 
         cy.get('@productThumbnail').find('.productcart')
-        .invoke('attr', 'title')
-        .should('include', 'Add to Cart')
-        
+            .invoke('attr', 'title')
+            .should('include', 'Add to Cart')
+
+    })
+
+    it.only("Calculate total of narmal and sale products", () => {
+        cy.visit('https://www.automationteststore.com/')
+        cy.get('.thumbnail').as('productThumbnail')
+
+        // cy.get('@productThumbnail')
+        //     .find('.oneprice')
+        //     .each(($el, index, $list) => {
+        //         cy.log($el.text())
+        //     })
+
+        cy.get('@productThumbnail')
+            .find('.oneprice')
+            .invoke('text')
+            .as('itemPrice')
+
+        cy.get('@productThumbnail')
+            .find('.pricenew')
+            .invoke('text')
+            .as('saleItemPrice')
+
+        var itemsTotal = 0
+        cy.get('@itemPrice')
+            .then($linkText => {
+                var itemPriceTotal = 0
+                //create an array
+                var itemPrice = $linkText.split('$')
+                //iterate througth the array
+                var i
+                // for loop
+                for (i = 0; i < itemPrice.length; i++) {
+                    cy.log(itemPrice[i])
+                    itemPriceTotal += Number(itemPrice[i])
+                }
+                itemsTotal += itemPriceTotal
+                cy.log("Non sale price items total: " + itemPriceTotal)
+            })
+
+        cy.get('@saleItemPrice')
+            .then($linkText => {
+                var saleItemPrice = $linkText.split('$')
+                var i
+                for (i = 0; i < saleItemPrice.length; i++) {
+                    cy.log(saleItemPrice[i])
+                    //saleItemPriceTotal += Number(saleItemPrice[i])
+                }
+            })
     })
 })
