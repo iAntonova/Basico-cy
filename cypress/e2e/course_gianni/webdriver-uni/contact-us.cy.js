81f793e6867babcdd06fcf7a81febada4565f22e
iAntonova <antonovava@gmail.com>
2023-03-23 11:24:28 +0200

    28: Child Windows

--- cypress/e2e/course_gianni/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/course_gianni/webdriver-uni/contact-us.cy.js
@@ -2,7 +2,13 @@
 
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        // cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.visit('http://www.webdriveruniversity.com')
+        /* jQuery removeAttr() Method
+         https://www.w3schools.com/jquery/html_removeattr.asp */
+        cy.get('#contact-us')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
@@ -16,7 +22,11 @@ describe("Test Contact Us form via WebdriverUni", () => {
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        // cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#contact-us')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
         cy.get('[name="first_name"]').type('Tom')
         cy.get('[name="last_name"]').type('Smith')
         cy.get('textarea.feedback-input').type('Lorem ipsom ... ')
