83dc1f2bcaa5926465a12c59f370906a21fbd6b9
iAntonova <antonovava@gmail.com>
2023-03-23 17:20:16 +0200

    33: Handling - Checkboxes - 1

---
+++ cypress/e2e/course_gianni/webdriver-uni/checkboxed.cy.js
@@ -0,0 +1,15 @@
+/* https://docs.cypress.io/api/commands/check#Syntax */
+
+/// <reference types="Cypress"/>
+
+describe("Verify checkboxes via webdriveruni", () => {
+    it("Check and validate checkbox", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#dropdown-checkboxes-radiobuttons')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+        cy.get('#checkboxes > :nth-child(1) > input').check()
+        cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
+    })
+})
+
