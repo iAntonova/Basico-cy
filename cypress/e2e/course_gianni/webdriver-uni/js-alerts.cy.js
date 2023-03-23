cc886c4a70f52c93fd72568ca0532bfc5124fcc2
iAntonova <antonovava@gmail.com>
2023-03-23 16:52:32 +0200

    31: Handling - Alerts

---
+++ cypress/e2e/course_gianni/webdriver-uni/js-alerts.cy.js
@@ -0,0 +1,61 @@
+/// <reference types="Cypress"/>
+/* https://docs.cypress.io/api/events/catalog-of-events#Event-Types */
+
+describe("Handle js alerts", () => {
+    it("Confirm js alerts contains the correct text", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#popup-alerts')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+        cy.get('#button1').click()
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('I am an alert box!')
+        })
+    })
+
+    it("Validate js confirm alerts box works correctly when clicking ok", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+
+        cy.get('#popup-alerts')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+        cy.get('#button4').click()
+        cy.on('window:alert', (str) => {
+            return true
+        })
+        cy.get('#confirm-alert-text').contains('You pressed OK!')
+    })
+
+    it("Validate js confirm alerts box works correctly when clicking cancel", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+
+        cy.get('#popup-alerts')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+        cy.get('#button4').click()
+        cy.on('window:confirm', (str) => {
+            return false
+        })
+        cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+    })
+
+    it.only("Validate js confirm alerts box using a stub", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+
+        cy.get('#popup-alerts')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('#button4').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+        }).then(() => {
+            return true
+        }).then(() => {
+            cy.get('#confirm-alert-text').contains('You pressed OK!')
+
+        })
+    })
+})
+
