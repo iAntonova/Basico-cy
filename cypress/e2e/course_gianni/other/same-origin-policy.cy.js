93a35b53afdd589653e0d1088f8ce20726f2c48d
iAntonova <antonovava@gmail.com>
2023-03-23 11:46:20 +0200

    29: Same Origin Policy

---
+++ cypress/e2e/course_gianni/other/same-origin-policy.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress"/>
+
+describe("Cypress web security", () => {
+    it("Validate visiting two different domains", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.visit('https://www.automationteststore.com/')
+    })
+
+    it("Validate visiting two different domains via users actions", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#automation-test-store').invoke('removeAttr', 'target').click()
+    })
+
+    it.only("Origin command", () => {
+        cy.origin('webdriveruniversity.com', () => {
+            cy.visit('/')
+        })
+        cy.origin('automationteststore.com', () => {
+            cy.visit('/')
+        })
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.visit('http://selectors.webdriveruniversity.com')
+    })
+})
+
