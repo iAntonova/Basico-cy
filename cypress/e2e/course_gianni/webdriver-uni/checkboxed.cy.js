d96ef77822c7ebf3d8c978a5870608e91385e7e5
iAntonova <antonovava@gmail.com>
2023-03-24 07:19:47 +0200

    33: Handling - Checkboxes - 3

--- cypress/e2e/course_gianni/webdriver-uni/checkboxed.cy.js
+++ cypress/e2e/course_gianni/webdriver-uni/checkboxed.cy.js
@@ -3,6 +3,7 @@
 /// <reference types="Cypress"/>
 
 describe("Verify checkboxes via webdriveruni", () => {
+
     it("Check and validate checkbox", () => {
         cy.visit('http://www.webdriveruniversity.com')
         cy.get('#dropdown-checkboxes-radiobuttons')
@@ -12,8 +13,29 @@ describe("Verify checkboxes via webdriveruni", () => {
         //cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
 
         cy.get('#checkboxes > :nth-child(1) > input').as('option-1')
-        cy.get('@option-1').check()
+        //cy.get('@option-1').check()
         cy.get('@option-1').check().should('be.checked')
+    })
+
+    it("Uncheck and validate checkbox", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#dropdown-checkboxes-radiobuttons')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+
+        cy.get('#checkboxes > :nth-child(5) > input').as('option-3')
+        cy.get('@option-3').uncheck()
+        .should('not.be.checked')
+    })
+
+    it("Check multiple checkboxes", () => {
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#dropdown-checkboxes-radiobuttons')
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
+
+        cy.get('input[type="checkbox"]').check(['option-1', 'option-2', 'option-3', 'option-4']).should('be.checked')
+      
     })
 })
 
