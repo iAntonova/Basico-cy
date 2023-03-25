a32024e6d157c88ea4db62c70b22e6d5b19d5542
iAntonova <antonovava@gmail.com>
2023-03-25 06:47:48 +0200

    39: Handling Data from Tables- 2

--- cypress/e2e/course_gianni/webdriver-uni/data-tables.cy.js
+++ cypress/e2e/course_gianni/webdriver-uni/data-tables.cy.js
@@ -7,12 +7,31 @@ describe("Handling datavia webdriveruni", () => {
 
     it("Calculate and assert the tatal age of all users", () => {
         var userDetails = []
+        let numb = 0
         cy.get('#thumbnail-1 td').each(($el, index, $list) => {
             userDetails[index] = $el.text()
         }).then(() => {
             var i
             for (i = 1; i < userDetails.length; i++) {
-                cy.log(userDetails[i])
+                if (Number(userDetails[i])) {
+                    numb += Number(userDetails[i])
+                }
+                // cy.log(userDetails[i])
+            }
+            cy.log("Found total age: " + numb)
+            expect(numb).to.eq(322)
+        })
+    });
+
+    it("Calculate and assert the age of a given user based on last name", () => {
+
+        cy.get('#thumbnail-1 tr td:nth-child(2)').each(($el, index, $list) => {
+            const text = $el.text()
+            if (text.includes("Woods")) {
+                cy.get('#thumbnail-1 tr td:nth-child(2)').eq(index).next().then(function (age) {
+                    const userAge = age.text()
+                    expect(userAge).to.eq("80")
+                })
             }
         })
     });
