1994515e5f5ad597813c16adbc46a24806c6cab4
iAntonova <antonovava@gmail.com>
2023-03-25 08:48:17 +0200

    43: Fixtures

--- cypress/e2e/course_gianni/automation-test-store/contact-us.cy.js
+++ cypress/e2e/course_gianni/automation-test-store/contact-us.cy.js
@@ -1,22 +1,31 @@
 /// <reference types="Cypress"/>
-
+/* https://docs.cypress.io/api/commands/fixture#Syntax */
 describe("Test Contact Us form via Automation Test Store", () => {
-    it.only("Should be able to submit a successful submission via contact us form", () => {
+
+    before(() => {
+        cy.fixture('userDetails').as('user')
+    })
+
+    it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit('https://www.automationteststore.com/')
         // 
         cy.get('a[href$="contact"]')
-        .click()
-        .then(function (linkText) {
-            cy.log("Clicked on link using text: " + linkText.text())
-        })
+            .click()
+            .then(function (linkText) {
+                cy.log("Clicked on link using text: " + linkText.text())
+            })
         // cy.xpath("//a[contains(@href, 'contact')]").click()
         // cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click()
         // cy.get('.info_links_footer > :nth-child(5) > a').click()
-        cy.get('#ContactUsFrm_first_name').type("Joe")
-        cy.get('#ContactUsFrm_email').type("joe_blogs112@gmail.com")
+
+        cy.get('@user').then((user) => {
+            cy.get('#ContactUsFrm_first_name').type(user.first_name)
+            cy.get('#ContactUsFrm_email').type(user.email)
+        })
+
         cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
         cy.get('#ContactUsFrm_enquiry').type("Lorem ipsom ... ")
-        cyb8.get('button[title="Submit"]').click()
+        cy.get('button[title="Submit"]').click()
         // cy.get('.col-md-6 > .btn').click()
 
         cy.get('.mb40 > :nth-child(3)')
--- cypress/e2e/course_gianni/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/course_gianni/webdriver-uni/contact-us.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="Cypress"/>
-
+/* https://docs.cypress.io/api/commands/fixture#Syntax */
 describe("Test Contact Us form via WebdriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+    before(function () {
+        cy.fixture('example').then(function (data) {
+            //this.data = data -- if this doesn't work use :
+            globalThis.data = data
+        })
+    })
+
+    beforeEach(() => {
         // cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
         cy.visit('http://www.webdriveruniversity.com')
         /* jQuery removeAttr() Method
@@ -9,26 +16,24 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.get('#contact-us')
             .invoke('removeAttr', 'target')
             .click({ force: true })
+    })
+
+    it("Should be able to submit a successful submission via contact us form", () => {
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
         //cy.get('#contact-us').click({force: true})
-        cy.get('[name="first_name"]').type('Joe')
-        cy.get('[name="last_name"]').type('Smith')
-        cy.get('[name="email"]').type('test@gmail.com')
+        cy.get('[name="first_name"]').type(data.first_name)
+        cy.get('[name="last_name"]').type(data.last_name)
+        cy.get('[name="email"]').type(data.email)
         cy.get('textarea.feedback-input').type('Lorem ipsom ... ')
         cy.get('[type="submit"]').click()
         cy.get('h1').should('have.text', 'Thank You for your Message!')
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        // cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
-        cy.visit('http://www.webdriveruniversity.com')
-        cy.get('#contact-us')
-            .invoke('removeAttr', 'target')
-            .click({ force: true })
-        cy.get('[name="first_name"]').type('Tom')
-        cy.get('[name="last_name"]').type('Smith')
+        cy.get('[name="first_name"]').type(data.first_name)
+        cy.get('[name="last_name"]').type(data.last_name)
         cy.get('textarea.feedback-input').type('Lorem ipsom ... ')
         cy.get('[type="submit"]').click()
         cy.get('body').contains('Error: all fields are required')
