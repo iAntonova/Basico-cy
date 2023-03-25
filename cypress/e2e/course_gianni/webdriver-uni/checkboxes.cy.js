596fbd471171f539ca21f9d76ac30d712df98b9d
iAntonova <antonovava@gmail.com>
2023-03-25 08:22:23 +0200

    42: Hooks

--- cypress/e2e/course_gianni/webdriver-uni/checkboxes.cy.js
+++ cypress/e2e/course_gianni/webdriver-uni/checkboxes.cy.js
@@ -3,12 +3,14 @@
 /// <reference types="Cypress"/>
 
 describe("Verify checkboxes via webdriveruni", () => {
-
-    it("Check and validate checkbox", () => {
+    beforeEach(() => {
         cy.visit('http://www.webdriveruniversity.com')
         cy.get('#dropdown-checkboxes-radiobuttons')
             .invoke('removeAttr', 'target')
             .click({ force: true })
+    })
+
+    it("Check and validate checkbox", () => {
         //cy.get('#checkboxes > :nth-child(1) > input').check()
         //cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
 
@@ -18,24 +20,13 @@ describe("Verify checkboxes via webdriveruni", () => {
     })
 
     it("Uncheck and validate checkbox", () => {
-        cy.visit('http://www.webdriveruniversity.com')
-        cy.get('#dropdown-checkboxes-radiobuttons')
-            .invoke('removeAttr', 'target')
-            .click({ force: true })
-
         cy.get('#checkboxes > :nth-child(5) > input').as('option-3')
         cy.get('@option-3').uncheck()
-        .should('not.be.checked')
+            .should('not.be.checked')
     })
 
     it("Check multiple checkboxes", () => {
-        cy.visit('http://www.webdriveruniversity.com')
-        cy.get('#dropdown-checkboxes-radiobuttons')
-            .invoke('removeAttr', 'target')
-            .click({ force: true })
-
         cy.get('input[type="checkbox"]').check(['option-1', 'option-2', 'option-3', 'option-4']).should('be.checked')
-      
     })
 })
 
--- cypress/e2e/course_gianni/webdriver-uni/radio-buttons.cy.js
+++ cypress/e2e/course_gianni/webdriver-uni/radio-buttons.cy.js
@@ -1,22 +1,19 @@
 /// <reference types="Cypress"/>
 
 describe("Verify radio buttons via webdriveruni", () => {
-
-    it("Check specific radio buttons", () => {
+    beforeEach(() => {
         cy.visit('http://www.webdriveruniversity.com')
         cy.get('#dropdown-checkboxes-radiobuttons')
             .invoke('removeAttr', 'target')
             .click({ force: true })
+    })
+
+    it("Check specific radio buttons", () => {
         cy.get('#radio-buttons').find('input[type="radio"]').first().check()
         cy.get('#radio-buttons').find('input[type="radio"]').eq(1).check()
     })
 
     it("Validate the states of specific radio buttons", () => {
-        cy.visit('http://www.webdriveruniversity.com')
-        cy.get('#dropdown-checkboxes-radiobuttons')
-            .invoke('removeAttr', 'target')
-            .click({ force: true })
-
         cy.get('input[value="lettuce"]').should('not.be.checked')
         cy.get('input[value="cabbage"]').should('be.disabled')
         cy.get('input[value="pumpkin"]').should('be.checked')
